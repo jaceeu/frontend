aae45970ff09cd3313dbd3647d78ddfdd929002f
jaceeu <jhancarlo.vasquez@tup.edu.ph>
2024-01-09 22:57:25 +0800

    added features

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 export const Navbar = () => {
+   const [menu,setMenu] = useState("shop")
   return (
     <div className='navbar'>
         <div className="nav-logo">
             <img src={logo} alt=""/>
-            <p>SHOPPER</p>
+            <p>SHOPBLISS</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<h/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<h/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<h/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<h/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
             <img src={cart_icon} alt="" />
+            <div className="nav-cart-count"></div>
         </div>
     </div>
   )

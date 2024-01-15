fb62326c6b6324773c1090672bb9ad12888c671d
jaceeu <jhancarlo.vasquez@tup.edu.ph>
2024-01-15 12:33:22 +0800

    loginsignup function adjusted and added display for sizes:prices

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import './Item.css'
-
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './Item.css';
 
 export const Item = (props) => {
   return (
     <div className='item'>
+      <Link to={`/product/₱{props.id}`}>
         <img src={props.image} alt="" />
-        <p>{props.name}</p>
-        <div className="item-prices">
-            <div className="item-prices-new">
-            ₱{props.new_price}
-            </div>
-            <div className="item-prices-old">
-            ₱{props.old_price}
-            </div>
+      </Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-prices-new">
+          ₱{props.new_price}
         </div>
+        <div className="item-prices-old">
+          ₱{props.old_price}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Item 
+export default Item;
